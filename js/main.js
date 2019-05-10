abbd2a43a7e1564a64b25749b029b0fec80d5bf9
Brian Han <bmhan319@hotmail.com>
2019-05-10 16:20:15 -0700

    created open and close commands in js for side menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function closeMenu() {
+  let closed = document.querySelector(".aside-window")
+  closed.style.left = -320;
+}
+
+function openMenu() {
+  let open = document.querySelector(".aside-window")
+  open.style.left = 0;
+}
