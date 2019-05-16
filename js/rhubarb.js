8be7d36ae1d3448a2534f0abab0be460fa17aadf
Brian Han <bmhan319@hotmail.com>
2019-05-16 15:40:09 -0700

    bottom slide card-close button is functional

--- js/rhubarb.js
+++ js/rhubarb.js
@@ -1,2 +1,15 @@
 //Making Default Video Volume low
 document.querySelector('.article-video').volume = 0.1;
+
+//Close Bottom Card
+function closePopUp() {
+  let bottomCard = document.querySelector(".visible")
+  bottomCard.style.opacity = 0
+}
+
+
+//Slide Bottom Card up at specific scrollbar height
+
+
+
+
