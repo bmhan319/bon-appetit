6d720bdef6488c97e4d523a333cc2dbf75bf8482
Brian Han <bmhan319@hotmail.com>
2019-05-16 16:01:14 -0700

    bottom slide card-pop-in functionality created

--- js/rhubarb.js
+++ js/rhubarb.js
@@ -7,9 +7,17 @@ function closePopUp() {
   bottomCard.style.opacity = 0
 }
 
-
 //Slide Bottom Card up at specific scrollbar height
-
+function openSlide() {
+  let body = document.querySelector("body")
+  let bottomCard = document.querySelector(".section-bottom-card")
+  if (body.scrollTop >= 2000) {
+    console.log(body.scrollTop)
+    bottomCard.style.bottom = 0
+  } else {
+    bottomCard.style.bottom = -186
+  }
+}
 
 
 
