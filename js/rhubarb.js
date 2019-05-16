7a794eefcf78500ee1ede21ea348a575e56e4288
Brian Han <bmhan319@hotmail.com>
2019-05-16 16:31:26 -0700

    bottom slide card-2nd popout in progress

--- js/rhubarb.js
+++ js/rhubarb.js
@@ -12,12 +12,52 @@ function openSlide() {
   let body = document.querySelector("body")
   let bottomCard = document.querySelector(".section-bottom-card")
   if (body.scrollTop >= 2000) {
-    console.log(body.scrollTop)
     bottomCard.style.bottom = 0
   } else {
-    bottomCard.style.bottom = -186
+    bottomCard.style.bottom = -190
   }
 }
 
+//Slide bottom Card down once you get  to the recipes section
+// get the element to animate
+var element = document.getElementById('section-readmore');
+var elementHeight = element.clientHeight;
 
+// listen for scroll event and call slide down function
+document.addEventListener('scroll', slideDown);
+
+// check if element is in view
+function inView() {
+  // get window height
+  var windowHeight = window.innerHeight;
+  // get number of pixels that the document is scrolled
+  var scrollY = window.scrollY || window.pageYOffset;
+  
+  // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+  var scrollPosition = scrollY + windowHeight;
+  // get element position (distance from the top of the page to the top of the element)
+  var elementPosition = element.getBoundingClientRect().top + scrollY;
+  
+  // is scroll position greater than element position? (is element in view?)
+  if (scrollPosition > elementPosition) {
+    return true;
+  }
+
+  return false;
+}
+
+// animate element when it is in view
+function slideDown() {
+  let bottomCard = document.querySelector(".section-bottom-card")
+  // is element in view?
+  if (inView()) {
+      // element is in view, add class to element
+      console.log(true)
+      bottomCard.style.backgroundColor = "red"
+      bottomCard.style.bottom = 50
+  } else {
+    bottomCard.style.backgroundColor = "white"
+    bottomCard.style.bottom = 0
+  }
+}
 
