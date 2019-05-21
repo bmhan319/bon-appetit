4d679792f12cd6a37adc14f49bacc8370ee4424b
Brian Han <bmhan319@hotmail.com>
2019-05-21 16:50:53 -0700

    slide animation created for two cards

---
+++ js/healthyish.js
@@ -0,0 +1,9 @@
+//Column Slide Animation
+
+function runSlide() {
+  let col1 = document.querySelector(".card1-col1")
+  let col2 = document.querySelector(".card1-col2")
+  
+  col1.classList.add("card1-col1-slide-down")
+  col2.classList.add("card1-col2-slide-up")
+}
