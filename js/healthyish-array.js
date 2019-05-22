820490e33a564fcaa14ef5f63e2bebdcfbc35717
Brian Han <bmhan319@hotmail.com>
2019-05-22 15:28:09 -0700

    started a siz object array of article topics

---
+++ js/healthyish-array.js
@@ -0,0 +1,38 @@
+let healthyArray = [
+  {category: "FOOD",
+   title: "We'll Be Drinking Co-Fermented Wines All Summer Long",
+   blurb: "Made by fermenting multiple grape varieties in one vessel, co-fermented wines are this summer's most delicious drink.",
+   image: "img/BA060119unfiltered.jpg",
+   alt: "wine and veggies"
+  },
+  {category: "CHEF",
+   title: "I Moved From Tokyo to the U.S. for Love But Became a Chef Instead",
+   blurb: "How Kuniko Yagi went from banker to udon waitress to owning her own restaurant in L.A.",
+   image: "img/PIKUNICO_KunikoYagi_0024.jpg",
+   alt: "Kuniko Yagi"
+  },
+  {category: "PROFILE",
+   title: "Adrienne Maree Brown on Why Joy Is a Form of Resistance",
+   blurb: "The author and social justice activist finds pleasure in slightly melted ice cream and a good solo dance party.",
+   image: "img/adrienne-maree1.jpg",
+   alt: "Adrienne Maree"
+  },
+  {category: "Chef",
+   title: "Inside the Mad-Science World of a Professional Fermentation Chef",
+   blurb: "How Caitlin Hata, the fermentation chef at The Manufactory, turns food waste into kimchi and more.",
+   image: "img/Caitlin-Tartine-lede.jpg",
+   alt: "Caitlin Tartine"
+  },
+  {category: "PROFILE",
+   title: "Lauren Friel Is Making Boston Drinking Culture a Little Less Bro-y",
+   blurb: "Her natural wine bar Rebel Rebel is unabashedly feminist and reliably good.",
+   image: "img/Healthyish-Lauren-Friel-Lede.jpg",
+   alt: "Lauren Friel"
+  },
+  {category: "TRENDS",
+   title: "Mānuka Honey Is a Wellness Wonder—But Your Jar Could Be a Fake",
+   blurb: "Here's how to make sure you're buying the real thing.",
+   image: "img/Manuka-Honey-2.jpg",
+   alt: "manuka honey"
+  } 
+]
