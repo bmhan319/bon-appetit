806ed9ffe0590770341cf98ae2c4991e4a486fcf
Brian Han <bmhan319@hotmail.com>
2019-05-21 17:30:42 -0700

    created 3rd card

--- js/healthyish.js
+++ js/healthyish.js
@@ -6,25 +6,7 @@ function runSlide() {
   
   col1.classList.add("card1-col1-slide-down")
   col2.classList.add("card1-col2-slide-up")
+
 }
 
 
-//bounce button
-$(document).ready(function() {
-animationClick('.bounceOut1','bounceOut');
-        
-function animationClick(element, animation){
-  element = $(element);
-  element.click(
-    function() {
-      element.addClass('animated ' + animation);
-      //wait for animation to finish before removing classes
-      window.setTimeout( function(){
-          element.removeClass('animated ' + animation);
-      }, 2000);
-    }
-  );
-};     
-        
-        
-});
