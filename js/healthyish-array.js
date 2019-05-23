70d40bce55a96233fac2ec0952f0b0fa8f94f01a
Brian Han <bmhan319@hotmail.com>
2019-05-22 17:36:41 -0700

    set initial styling on all cards

--- js/healthyish-array.js
+++ js/healthyish-array.js
@@ -21,7 +21,7 @@ let healthArray = [
    alt: "Adrienne Maree"
   },
   {index: 12,
-   category: "Chef",
+   category: "CHEF",
    title: "Inside the Mad-Science World of a Professional Fermentation Chef",
    blurb: "How Caitlin Hata, the fermentation chef at The Manufactory, turns food waste into kimchi and more.",
    image: "img/Caitlin-Tartine-lede.jpg",
@@ -40,24 +40,63 @@ let healthArray = [
    blurb: "Here's how to make sure you're buying the real thing.",
    image: "img/Manuka-Honey-2.jpg",
    alt: "manuka honey"
-  } 
-]
-
-document.getElementById("mainContainer").addEventListener("load", loadHTML())
-
-function loadHTML() {
-  for (var i = 0; i < 6; i++) {
-    let image1 = document.querySelector("#card" + (i + 1) + "Image1") 
-    let title = document.querySelector("#card" + (i + 1) + "Title") 
-    let blurb = document.querySelector("#card" + (i + 1) + "Blurb")
-    let category = document.querySelector("#card" + (i + 1) + "Category")
-    
-    image1.setAttribute("src", healthArray[i].image)
-    image1.setAttribute("alt", healthArray[i].alt)
-    title.innerHTML = healthArray[i].title
-    blurb.innerHTML = healthArray[i].blurb
-    category.innerHTML = healthArray[i].category
+  },
+  {index: 9,
+   category: "RECIPE",
+   title: "If You Come Over for Dinner, We’re Having Enchiladas",
+   blurb: "Cover me in salsa verde and bury me in Monterey Jack",
+   image: "img/HLY-Veggie-Enchilada-Vertical.jpg",
+   alt: "veggie enchilada"
+  },
+  {index: 8,
+   category: "ESSAY",
+   title: "Yes, I'm In School for Nutrition. No, I Don't Want to Hear About Your Diet.",
+   blurb: "I’m not going to tell you what to eat, but I will tell you to stop stressing about it.",
+   image: "img/HLY-nutrition-school.jpg",
+   alt: "nutrition"
+  },
+  {index: 7,
+   category: "RECIPE",
+   title: "Put This Citrusy Asparagus Stir-Fry on the Top of Your Spring Bucket List",
+   blurb: "Asparagus season is beautiful and brief, so make this bright stir-fry while you can.",
+   image: "img/HLY-Asparagus-Vertical.jpg",
+   alt: "citrus asparagus stir fry"
+  },
+  {index: 6,
+   category: "PRODUCTS",
+   title: "These Linen Napkins Make Me Feel Way Fancier Than I Am",
+   blurb: "They're big enough to handle a messy, saucy meal without being unwieldy—and easy to throw in the hamper when you're done.",
+   image: "img/loves-it-napkins3_lg.jpg",
+   alt: "linen napkins"
+  },
+  {index: 5,
+   category: "RECIPE",
+   title: "Why Scientists See Fiber As More Important Than Ever Before",
+   blurb: "Fiber used to be the most boring nutrient we eat. Then scientists started looking at our microbes.",
+   image: "img/HLY-Fiber-1-Big.jpg",
+   alt: "green veggies"
+  },
+  {index: 4,
+   category: "PRODUCTS",
+   title: "This Clean-Beauty Foundation Is Made For People Who Don't Like Foundation",
+   blurb: "Kosas tinted face oil delivers nutrients to your skin and stays sheer enough for your natural tone to come through.",
+   image: "img/20190416-STAFF-HEADSHOTS4817.jpg",
+   alt: "Kosas Face Oil"
+  },
+  {index: 3,
+   category: "LIFESTYLE",
+   title: "Welcome to the Underground World of Healthy Clubbing",
+   blurb: "Clubs in NYC are incorporating wellness practices to help ravers find self-care after dark.",
+   image: "img/healthyish-partying-lede.jpg",
+   alt: "healthyish partying"
+  },
+  {index: 2,
+   category: "PROFILE",
+   title: "Christina Lecki On Leaving Reynard and What She's Doing Next",
+   blurb: "Lecki is trying to get to the root of who she is and where she wants to be.",
+   image: "img/HLY-Christina-Lecki.jpg",
+   alt: "Christina Lecki"
   }
-}
+]
 
 
--- js/healthyish.js
+++ js/healthyish.js
@@ -1,4 +1,19 @@
+document.getElementById("mainContainer").addEventListener("load", loadHTML())
 
+function loadHTML() {
+  for (var i = 0; i < 15; i++) {
+    let image1 = document.querySelector("#card" + (i + 1) + "Image1") 
+    let title = document.querySelector("#card" + (i + 1) + "Title") 
+    let blurb = document.querySelector("#card" + (i + 1) + "Blurb")
+    let category = document.querySelector("#card" + (i + 1) + "Category")
+    
+    image1.setAttribute("src", healthArray[i].image)
+    image1.setAttribute("alt", healthArray[i].alt)
+    title.innerHTML = healthArray[i].title
+    blurb.innerHTML = healthArray[i].blurb
+    category.innerHTML = healthArray[i].category
+  }
+}
 
 
 
