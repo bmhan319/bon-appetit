12d5bb79739cc5b23a21e2be7e680b3120851ef9
Brian Han <bmhan319@hotmail.com>
2019-05-24 16:53:12 -0700

    updated font type

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function closeMenu() {
   let closed = document.querySelector(".aside-window")
-  closed.style.left = '-100vw';
+  closed.style.left = '-150vw'
   //closed.style.left = -320;  changes to this at 768pixels wide
   
 }
 
 function openMenu() {
   let open = document.querySelector(".aside-window")
-  open.style.left = 0;
+  open.style.left = 0
 }
