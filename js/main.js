835b249aedfecf137f5ede9398d1c6766958916b
Brian Han <bmhan319@hotmail.com>
2019-05-29 00:14:18 -0700

    gradient shadow on video

--- js/main.js
+++ js/main.js
@@ -11,11 +11,14 @@ function openMenu() {
 
 function startVideo(num) {
   let video = document.querySelector(".video" + num)
+  let videoBox = document.querySelector(".video" + num + "-box")
   let thumbNail = document.querySelector(".video" + num + "-tn")
   let playButton = document.querySelector(".play-button" + num)
 
   video.classList.add("video" + num + "-on")
   video.classList.remove("video" + num + "-off")
+  videoBox.classList.add("video" + num + "-box-on")
+  videoBox.classList.remove("video" + num + "-box-off")
   thumbNail.classList.add("video" + num + "-tn-off")
   thumbNail.classList.remove("video" + num + "-tn")
   playButton.classList.add("play-button" + num + "-off")
