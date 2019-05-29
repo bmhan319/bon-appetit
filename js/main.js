cc726d0a41d62e31a5a9ca9b9b11a56fa5993f53
Brian Han <bmhan319@hotmail.com>
2019-05-28 21:44:30 -0700

    video preview complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 function closeMenu() {
   let closed = document.querySelector(".aside-window")
   closed.style.left = '-150vw'
-  //closed.style.left = -320;  changes to this at 768pixels wide
   
 }
 
 function openMenu() {
   let open = document.querySelector(".aside-window")
   open.style.left = 0
+}
+
+function startVideo(num) {
+  let video = document.querySelector(".video" + num)
+  let thumbNail = document.querySelector(".video" + num + "-tn")
+  let playButton = document.querySelector(".play-button" + num)
+
+  video.classList.add("video" + num + "-on")
+  video.classList.remove("video" + num + "-off")
+  thumbNail.classList.add("video" + num + "-tn-off")
+  thumbNail.classList.remove("video" + num + "-tn")
+  playButton.classList.add("play-button" + num + "-off")
+  playButton.classList.remove("play-button" + num)
 }
