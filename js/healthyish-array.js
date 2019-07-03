3f6a39ffa6316819955f8c089ec437abc1318ed9
Brian Han <bmhan319@hotmail.com>
2019-07-03 16:59:42 -0700

    adjusted card2 image sizing

--- js/healthyish-array.js
+++ js/healthyish-array.js
@@ -4,98 +4,126 @@ let healthArray = [
    title: "We'll Be Drinking Co-Fermented Wines All Summer Long",
    blurb: "Made by fermenting multiple grape varieties in one vessel, co-fermented wines are this summer's most delicious drink.",
    image: "img/BA060119unfiltered.jpg",
-   alt: "wine and veggies"
+   alt: "wine and veggies",
+   bgColor1:"",
+   bgColor2:"#ffa67a"
   },
   {index: 14,
    category: "CHEF",
    title: "I Moved From Tokyo to the U.S. for Love But Became a Chef Instead",
    blurb: "How Kuniko Yagi went from banker to udon waitress to owning her own restaurant in L.A.",
    image: "img/PIKUNICO_KunikoYagi_0024.jpg",
-   alt: "Kuniko Yagi"
+   alt: "Kuniko Yagi",
+   bgColor1:"#f4f2e7",
+   bgColor2:"#dedaca"
   },
   {index: 13,
    category: "PROFILE",
    title: "Adrienne Maree Brown on Why Joy Is a Form of Resistance",
    blurb: "The author and social justice activist finds pleasure in slightly melted ice cream and a good solo dance party.",
    image: "img/adrienne-maree1.jpg",
-   alt: "Adrienne Maree"
+   alt: "Adrienne Maree",
+   bgColor1:"",
+   bgColor2:"#fbf0a0"
   },
   {index: 12,
    category: "CHEF",
    title: "Inside the Mad-Science World of a Professional Fermentation Chef",
    blurb: "How Caitlin Hata, the fermentation chef at The Manufactory, turns food waste into kimchi and more.",
    image: "img/Caitlin-Tartine-lede.jpg",
-   alt: "Caitlin Tartine"
+   alt: "Caitlin Tartine",
+   bgColor1:"",
+   bgColor2:"#daf3f5"
   },
   {index: 11,
    category: "PROFILE",
    title: "Lauren Friel Is Making Boston Drinking Culture a Little Less Bro-y",
    blurb: "Her natural wine bar Rebel Rebel is unabashedly feminist and reliably good.",
    image: "img/Healthyish-Lauren-Friel-Lede.jpg",
-   alt: "Lauren Friel"
+   alt: "Lauren Friel",
+   bgColor1:"#d1f9f0",
+   bgColor2:""
   },
   {index: 10,
    category: "TRENDS",
    title: "Mānuka Honey Is a Wellness Wonder—But Your Jar Could Be a Fake",
    blurb: "Here's how to make sure you're buying the real thing.",
    image: "img/Manuka-Honey-2.jpg",
-   alt: "manuka honey"
+   alt: "manuka honey",
+   bgColor1:"",
+   bgColor2:"#ffffff"
   },
   {index: 9,
    category: "RECIPE",
    title: "If You Come Over for Dinner, We’re Having Enchiladas",
    blurb: "Cover me in salsa verde and bury me in Monterey Jack",
    image: "img/HLY-Veggie-Enchilada-Vertical.jpg",
-   alt: "veggie enchilada"
+   alt: "veggie enchilada",
+   bgColor1:"",
+   bgColor2:""
   },
   {index: 8,
    category: "ESSAY",
    title: "Yes, I'm In School for Nutrition. No, I Don't Want to Hear About Your Diet.",
    blurb: "I’m not going to tell you what to eat, but I will tell you to stop stressing about it.",
    image: "img/HLY-nutrition-school.jpg",
-   alt: "nutrition"
+   alt: "nutrition",
+   bgColor1:"",
+   bgColor2:""
   },
   {index: 7,
    category: "RECIPE",
    title: "Put This Citrusy Asparagus Stir-Fry on the Top of Your Spring Bucket List",
    blurb: "Asparagus season is beautiful and brief, so make this bright stir-fry while you can.",
    image: "img/HLY-Asparagus-Vertical.jpg",
-   alt: "citrus asparagus stir fry"
+   alt: "citrus asparagus stir fry",
+   bgColor1:"",
+   bgColor2:""
   },
   {index: 6,
    category: "PRODUCTS",
    title: "These Linen Napkins Make Me Feel Way Fancier Than I Am",
    blurb: "They're big enough to handle a messy, saucy meal without being unwieldy—and easy to throw in the hamper when you're done.",
    image: "img/loves-it-napkins3_lg.jpg",
-   alt: "linen napkins"
+   alt: "linen napkins",
+   bgColor1:"",
+   bgColor2:""
   },
   {index: 5,
    category: "RECIPE",
    title: "Why Scientists See Fiber As More Important Than Ever Before",
    blurb: "Fiber used to be the most boring nutrient we eat. Then scientists started looking at our microbes.",
    image: "img/HLY-Fiber-1-Big.jpg",
-   alt: "green veggies"
+   alt: "green veggies",
+   bgColor1:"",
+   bgColor2:""
   },
   {index: 4,
    category: "PRODUCTS",
    title: "This Clean-Beauty Foundation Is Made For People Who Don't Like Foundation",
    blurb: "Kosas tinted face oil delivers nutrients to your skin and stays sheer enough for your natural tone to come through.",
    image: "img/20190416-STAFF-HEADSHOTS4817.jpg",
-   alt: "Kosas Face Oil"
+   alt: "Kosas Face Oil",
+   bgColor1:"",
+   bgColor2:""
   },
   {index: 3,
    category: "LIFESTYLE",
    title: "Welcome to the Underground World of Healthy Clubbing",
    blurb: "Clubs in NYC are incorporating wellness practices to help ravers find self-care after dark.",
    image: "img/healthyish-partying-lede.jpg",
-   alt: "healthyish partying"
+   alt: "healthyish partying",
+   bgColor1:"",
+   bgColor2:""
   },
   {index: 2,
    category: "PROFILE",
    title: "Christina Lecki On Leaving Reynard and What She's Doing Next",
    blurb: "Lecki is trying to get to the root of who she is and where she wants to be.",
    image: "img/HLY-Christina-Lecki.jpg",
-   alt: "Christina Lecki"
+   alt: "Christina Lecki",
+   bgColor1:"",
+   bgColor2:""
   }
 ]
 
