856322ae804b368bbd648d28f13a23c578051364
Brian Han <bmhan319@hotmail.com>
2019-07-15 16:42:10 -0700

    adjusted slideshow to go vertically up, woking on right column to go down

--- js/test.js
+++ js/test.js
@@ -1,8 +1,9 @@
-const carouselSlide = document.querySelector(".carousel-slide")
-const carouselImages = document.querySelectorAll(".carousel-slide div")
+const carouselSlide = document.querySelector(".carousel-slideUp")
+const carouselImages = document.querySelectorAll(".carousel-slideUp div")
 const next = document.querySelector(".next")
 let counter = 0;
-const size = carouselImages[0].clientWidth
+//const size = carouselImages[0].clientWidth
+const size = carouselImages[0].clientHeight
 
 next.addEventListener('click',()=>{
   if (counter >= carouselImages.length -1) {
@@ -10,13 +11,13 @@ next.addEventListener('click',()=>{
   }
   carouselSlide.style.transition = 'transform 0.4s linear'
   counter++
-  carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"
+  carouselSlide.style.transform = "translateY(" + (-size * counter) + "px)"
 })
 
 carouselSlide.addEventListener("transitionend", ()=>{
   if(carouselImages[counter].id === "lastClone"){
     carouselSlide.style.transition = "none"
     counter = 0
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)"
+    carouselSlide.style.transform = "translateY(" + (-size * counter) + "px)"
   }
 })
