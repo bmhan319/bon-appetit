0ea7faf41863f9790940ee6fbc8d58caf527e069
Brian Han <bmhan319@hotmail.com>
2019-07-15 17:00:47 -0700

    created downward slideshow for right column

--- js/test.js
+++ js/test.js
@@ -1,23 +1,42 @@
-const carouselSlide = document.querySelector(".carousel-slideUp")
-const carouselImages = document.querySelectorAll(".carousel-slideUp div")
-const next = document.querySelector(".next")
-let counter = 0;
-//const size = carouselImages[0].clientWidth
-const size = carouselImages[0].clientHeight
+const carouselSlideUp = document.querySelector(".carousel-slideUp")
+const carouselSlideDown = document.querySelector(".carousel-slideDown")
 
+const carouselImagesUp = document.querySelectorAll(".carousel-slideUp div")
+const carouselImagesDown = document.querySelectorAll(".carousel-slideDown div")
+
+const next = document.querySelector(".next")
+let counter = 0
+let counterDown =  4
+const size = carouselImagesUp[0].clientHeight
+
+
+//Next Button Click Action
 next.addEventListener('click',()=>{
-  if (counter >= carouselImages.length -1) {
+  if (counter >= carouselImagesUp.length -1) {
     return
   }
-  carouselSlide.style.transition = 'transform 0.4s linear'
+  carouselSlideUp.style.transition = 'transform 0.4s linear'
+  carouselSlideDown.style.transition = 'transform 0.4s linear'
   counter++
-  carouselSlide.style.transform = "translateY(" + (-size * counter) + "px)"
+  counterDown--
+  carouselSlideUp.style.transform = "translateY(" + (-size * counter) + "px)"
+  carouselSlideDown.style.transform = "translateY(" + (size * counter) + "px)"
 })
 
-carouselSlide.addEventListener("transitionend", ()=>{
-  if(carouselImages[counter].id === "lastClone"){
-    carouselSlide.style.transition = "none"
+
+//Looping slideshow around to first slide
+carouselSlideUp.addEventListener("transitionend", ()=>{
+  if(carouselImagesUp[counter].id === "lastClone"){
+    carouselSlideUp.style.transition = "none"
     counter = 0
-    carouselSlide.style.transform = "translateY(" + (-size * counter) + "px)"
+    carouselSlideUp.style.transform = "translateY(" + (-size * counter) + "px)"
+  }
+})
+
+carouselSlideDown.addEventListener("transitionend", ()=>{
+  if(carouselImagesDown[counterDown].id === "lastClone"){
+    carouselSlideDown.style.transition = "none"
+    counterDown = 4
+    carouselSlideDown.style.transform = "translateY(" + (size * counter) + "px)"
   }
 })
