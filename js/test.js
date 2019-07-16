39c574110444227b7dd2bd20a2a00d1eeba4af47
Brian Han <bmhan319@hotmail.com>
2019-07-15 17:31:21 -0700

    added 15 slides in both columns

--- js/test.js
+++ js/test.js
@@ -1,3 +1,14 @@
+document.getElementById("mainContainer").addEventListener("load", loadHTML())
+
+function loadHTML() {
+  for (var i = 0; i < 1; i++) {
+    let image1 = document.querySelector("#card" + (i + 1) + "Image1") 
+    image1.setAttribute("src", healthArray[i].image)
+    image1.setAttribute("alt", healthArray[i].alt)
+  }
+}
+
+
 const carouselSlideUp = document.querySelector(".carousel-slideUp")
 const carouselSlideDown = document.querySelector(".carousel-slideDown")
 
@@ -6,7 +17,7 @@ const carouselImagesDown = document.querySelectorAll(".carousel-slideDown div")
 
 const next = document.querySelector(".next")
 let counter = 0
-let counterDown =  4
+let counterDown =  15
 const size = carouselImagesUp[0].clientHeight
 
 
@@ -36,7 +47,7 @@ carouselSlideUp.addEventListener("transitionend", ()=>{
 carouselSlideDown.addEventListener("transitionend", ()=>{
   if(carouselImagesDown[counterDown].id === "lastClone"){
     carouselSlideDown.style.transition = "none"
-    counterDown = 4
+    counterDown = 15  //this number should change to however many slides there are
     carouselSlideDown.style.transform = "translateY(" + (size * counter) + "px)"
   }
 })
