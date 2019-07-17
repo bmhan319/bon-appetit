4cc66da9fdaed79c67b8cc321919dff7c2ff77db
Brian Han <bmhan319@hotmail.com>
2019-07-16 19:55:28 -0700

    added in the final clone panels to switch seamlesslessly back to the first panels

--- js/test.js
+++ js/test.js
@@ -1,17 +1,25 @@
 document.getElementById("mainContainer").addEventListener("load", loadHTML())
 
 function loadHTML() {
-  for (var i = 0; i < 14; i++) {
+  for (var i = 0; i < 15; i++) {
     let image1 = document.querySelector("#card" + (i + 1) + "Image1") 
     let title = document.querySelector("#card" + (i + 1) + "Title") 
     let blurb = document.querySelector("#card" + (i + 1) + "Blurb")
     let category = document.querySelector("#card" + (i + 1) + "Category")
     
-    image1.setAttribute("src", healthArray[i].image)
-    image1.setAttribute("alt", healthArray[i].alt)
-    title.innerHTML = healthArray[i].title
-    blurb.innerHTML = healthArray[i].blurb
-    category.innerHTML = healthArray[i].category
+    if (image1.parentElement.parentElement.id == "lastClone") {
+      image1.setAttribute("src", healthArray[0].image)
+      image1.setAttribute("alt", healthArray[0].alt)
+      title.innerHTML = healthArray[0].title
+      blurb.innerHTML = healthArray[0].blurb
+      category.innerHTML = healthArray[0].category
+    } else {
+      image1.setAttribute("src", healthArray[i].image)
+      image1.setAttribute("alt", healthArray[i].alt)
+      title.innerHTML = healthArray[i].title
+      blurb.innerHTML = healthArray[i].blurb
+      category.innerHTML = healthArray[i].category
+    }
   }
 }
 
