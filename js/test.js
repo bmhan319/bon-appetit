3c517927732b342e96f900badce6d8a1cf881980
Brian Han <bmhan319@hotmail.com>
2019-07-16 17:06:14 -0700

    added in html code for each slideshow panel and tested to make sure that slideshow still functioned

--- js/test.js
+++ js/test.js
@@ -1,19 +1,27 @@
 document.getElementById("mainContainer").addEventListener("load", loadHTML())
 
 function loadHTML() {
-  for (var i = 0; i < 1; i++) {
+  for (var i = 0; i < 14; i++) {
     let image1 = document.querySelector("#card" + (i + 1) + "Image1") 
+    let title = document.querySelector("#card" + (i + 1) + "Title") 
+    let blurb = document.querySelector("#card" + (i + 1) + "Blurb")
+    let category = document.querySelector("#card" + (i + 1) + "Category")
+    
     image1.setAttribute("src", healthArray[i].image)
     image1.setAttribute("alt", healthArray[i].alt)
+    title.innerHTML = healthArray[i].title
+    blurb.innerHTML = healthArray[i].blurb
+    category.innerHTML = healthArray[i].category
   }
 }
 
 
+//Slideshow 
 const carouselSlideUp = document.querySelector(".carousel-slideUp")
 const carouselSlideDown = document.querySelector(".carousel-slideDown")
 
-const carouselImagesUp = document.querySelectorAll(".carousel-slideUp div")
-const carouselImagesDown = document.querySelectorAll(".carousel-slideDown div")
+const carouselImagesUp = document.querySelectorAll(".carousel-slideUp .divs")
+const carouselImagesDown = document.querySelectorAll(".carousel-slideDown .divs")
 
 const next = document.querySelector(".next")
 let counter = 0
