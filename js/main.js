eafe84c3d993c9f6645314fe8b515a0e975d05a7
Brian Han <bmhan319@hotmail.com>
2019-08-02 14:38:02 -0700

    fixed the expanding navbar on main and healthyish page, prevented menu from being visible on resize

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 function closeMenu() {
-  let closed = document.querySelector(".aside-window")
-  closed.style.left = '-150vw'
-  
+  const closed = document.querySelector(".aside-window");
+  closed.style.left = "-150vw";
 }
 
 function openMenu() {
-  let open = document.querySelector(".aside-window")
-  open.style.left = 0
+  const open = document.querySelector(".aside-window");
+  open.style.left = 0;
+  open.style.opacity = 1;
 }
 
 function startVideo(num) {
-  let video = document.querySelector(".video" + num)
-  let videoBox = document.querySelector(".video" + num + "-box")
-  let thumbNail = document.querySelector(".video" + num + "-tn")
-  let playButton = document.querySelector(".play-button" + num)
+  let video = document.querySelector(".video" + num);
+  let videoBox = document.querySelector(".video" + num + "-box");
+  let thumbNail = document.querySelector(".video" + num + "-tn");
+  let playButton = document.querySelector(".play-button" + num);
 
-  video.classList.add("video" + num + "-on")
-  video.classList.remove("video" + num + "-off")
-  videoBox.classList.add("video" + num + "-box-on")
-  videoBox.classList.remove("video" + num + "-box-off")
-  thumbNail.classList.add("video" + num + "-tn-off")
-  thumbNail.classList.remove("video" + num + "-tn")
-  playButton.classList.add("play-button" + num + "-off")
-  playButton.classList.remove("play-button" + num)
+  video.classList.add("video" + num + "-on");
+  video.classList.remove("video" + num + "-off");
+  videoBox.classList.add("video" + num + "-box-on");
+  videoBox.classList.remove("video" + num + "-box-off");
+  thumbNail.classList.add("video" + num + "-tn-off");
+  thumbNail.classList.remove("video" + num + "-tn");
+  playButton.classList.add("play-button" + num + "-off");
+  playButton.classList.remove("play-button" + num);
 }
